ac9862516067fdac654918605bb3725112c79cc5
Drew Proebstel <d.proebstel@gmail.com>
2022-06-13 09:49:50 -0700

    book poro

--- app/facades/book_facade.rb
+++ app/facades/book_facade.rb
@@ -1,7 +1,7 @@
 class BookFacade
   def self.find_books(location,quantity)
   	book_data = BookService.get_books(location,quantity)
-    books = book_data.map do |book|
+    books = book_data[:docs].map do |book|
       Book.new(book)
     end
   end
